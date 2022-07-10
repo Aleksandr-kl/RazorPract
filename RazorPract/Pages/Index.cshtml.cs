f268e0c5b69051b24f91341e76733d4e9d02ee0c
kalen <kalen@DESKTOP-R3F2VG6>
2022-07-10 14:14:58 +0300

    Добавьте файлы проекта.

---
+++ RazorPract/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPract.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+
+        public void OnGet()
+        {
+            Age = 33;
+            Name = "Sasha";
+        }
+    }
+}
---
+++ RazorPract/Pages/Product.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPract.Pages
+{
+    public class ProductModel : PageModel
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public bool IsCorrect { get; set; } = true; public void OnGet(string name, int? age)
+        {
+            if (age == null || age < 1 || age > 110 || string.IsNullOrEmpty(name))
+            {
+                IsCorrect = false; return;
+            }
+            Age = age; Name = name;
+        }
+    }
+}
